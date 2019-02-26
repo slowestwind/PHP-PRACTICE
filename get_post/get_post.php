988d167dcca1c81cc7d9233043e3c67329718f9e
slowestwind <slowestwind@gmail.com>
2019-02-26 11:11:14 +0530

    update

---
+++ get_post/get_post.php
@@ -0,0 +1,66 @@
+<!--
+  Author: Arvind Sharma
+
+ -->
+
+ <!-- php code goes here -->
+ <?php
+
+
+
+ // this is for get method which is not secure since information can be guess
+ // throuth the url of the because get method use url to send data
+
+  if (isset($_GET['name'])) {
+    // print_r($_GET);
+    $name=htmlentities($_GET['name']);
+    echo $name;
+  }
+
+/*
+// This is for post method which is more secure than get method because data is
+// not send through url.
+  if (isset($_POST['name'])) {
+    // code...
+    $name = htmlentities($_POST['name']);
+     print_r($name);
+    // echo $name;
+
+  }
+
+  */
+
+    // full info with key and value can be seen by
+    echo $_SERVER['QUERY_STRING'];
+  ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>GET | POST</title>
+  </head>
+  <body>
+    <!-- get user data -->
+    <form class="main-form" action="get_post.php" method="GET">
+      <label>Name</label>
+      <input type="text" name="name" value="Enter Name">
+      <label>Email</label>
+      <input type="text" name="email" value="Enter Email">
+      <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <ul>
+
+      <!-- code stunt -->
+      <li>
+        <a href="get_post.php?name=Arvind">Arvind</a>
+      </li>
+      <li>
+        <a href="get_post.php?name=Suraj">Suraj</a>
+      </li>
+      <?php echo "{$name}'s Profile"; ?>
+    </ul>
+
+  </body>
+</html>
--- website2/index.php
+++ website2/index.php
@@ -9,10 +9,23 @@
   <body>
     <div class="container">
 
-      <h1>server and file info </h1>
+      <h1>Server Info</h1>
       <?php if ($server): ?>
         <ul class="list-group">
           <?php foreach ($server as $key => $value) : ?>
+            <li class="list-group-item">
+              <strong><?php echo $key ?></strong>
+              <?php echo $value ?>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      <?php endif; ?>
+
+      <!-- client side info -->
+      <h1>Client Info </h1>
+      <?php if ($client): ?>
+        <ul class="list-group">
+          <?php foreach ($client as $key => $value) : ?>
             <li class="list-group-item">
               <strong><?php echo $key ?></strong>
               <?php echo $value ?>
