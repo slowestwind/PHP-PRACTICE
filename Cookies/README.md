0e407bb7dd2a15611f021e9d1e9755047a459bce
slowestwind <slowestwind@gmail.com>
2019-03-04 21:27:58 +0530

    coockie file

---
+++ Cookies/README.md
@@ -0,0 +1,21 @@
+# DIFFERENCE B/W SESSION AND COOKIE
+
+## SESSION
+  ```
+   Session data store on the server
+   will not set expression
+  ```
+
+## COOKIE
+    ```
+    COOKIE data store on the browser
+    with COOKIE you can set expression
+
+    ```
+
+## Function
+  ```
+  To set cookie the required parameters
+  setcookie('cookie name', value, expression);
+
+  ```
---
+++ Cookies/index.php
@@ -0,0 +1,25 @@
+<!-- php code -->
+<?php
+
+
+ ?>
+
+<!-- html code -->
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- form -->
+    <form class="form-class" action="index.php" method="post">
+      <label>Name</label><br>
+      <input type="text" name="name" value=""><br>
+      <label>Email</label><br>
+      <input type="text" name="email" value=""><br>
+      <input type="text" name="submit" value="Submit">
+    </form>
+
+  </body>
+</html>
